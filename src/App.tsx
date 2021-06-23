9f825cedeb1b7ec6da90910ba7fdb834c125be11
ibsiany <ibsianyd@gmail.com>
2021-06-22 21:53:39 -0300

    aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import {Button} from "./components/Button";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Route, BrowserRouter } from "react-router-dom";
+
+import {AuthContextProvider} from './contexts/AuthContext'
 
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button text="Clique aqui"/>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
